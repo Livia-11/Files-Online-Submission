41786a058f737d0e6ff74df8e2f68621793160b6
Livia-11 <kirezilivia@gmail.com>
2025-02-09 16:38:59 +0200

    Updating the model/User

--- src/main/java/com/submission/mis/filesonlinesubmission/model/User.java
+++ src/main/java/com/submission/mis/filesonlinesubmission/model/User.java
@@ -1,4 +1,60 @@
 package com.submission.mis.filesonlinesubmission.model;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
 public class User {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String email;
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private UserType type;
+
+    public enum UserType { STUDENT, INSTRUCTOR }
+
+    public User() {}
+
+    public User(Long id, String email, String password, UserType type) {
+        this.id = id;
+        this.email = email;
+        this.password = password;
+        this.type = type;
+    }
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public UserType getType() {
+        return type;
+    }
+
+    public void setType(UserType type) {
+        this.type = type;
+    }
 }
+
