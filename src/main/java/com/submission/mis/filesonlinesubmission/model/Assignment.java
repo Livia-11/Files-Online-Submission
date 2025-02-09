73b29305a75b3a1a5c1889d6029e733aad2d6f5c
Livia-11 <kirezilivia@gmail.com>
2025-02-09 16:41:33 +0200

    Updating the model/Assignment

--- src/main/java/com/submission/mis/filesonlinesubmission/model/Assignment.java
+++ src/main/java/com/submission/mis/filesonlinesubmission/model/Assignment.java
@@ -1,4 +1,69 @@
 package com.submission.mis.filesonlinesubmission.model;
 
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "assignments")
 public class Assignment {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    private String description;
+
+    private LocalDateTime deadline;
+
+    @ManyToOne
+    @JoinColumn(name = "instructor_id")
+    private User instructor;
+
+    public Assignment() {}
+    public Assignment(String title, String description, LocalDateTime deadline, User instructor) {
+        this.title = title;
+        this.description = description;
+        this.deadline = deadline;
+        this.instructor = instructor;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDateTime getDeadline() {
+        return deadline;
+    }
+
+    public void setDeadline(LocalDateTime deadline) {
+        this.deadline = deadline;
+    }
+
+    public User getInstructor() {
+        return instructor;
+    }
+
+    public void setInstructor(User instructor) {
+        this.instructor = instructor;
+    }
 }
