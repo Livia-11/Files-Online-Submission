44eb4bf06ea7f1fd9f3567514b9671c21dc2a276
Livia-11 <kirezilivia@gmail.com>
2025-02-09 17:05:30 +0200

    Updates

--- src/main/java/com/submission/mis/filesonlinesubmission/dao/AssignmentDao.java
+++ src/main/java/com/submission/mis/filesonlinesubmission/dao/AssignmentDao.java
@@ -1,4 +1,16 @@
 package com.submission.mis.filesonlinesubmission.dao;
 
+import org.hibernate.Session;
+import org.hibernate.sql.ast.tree.update.Assignment;
+
 public class AssignmentDao {
+    public void saveAssignment(Assignment assignment) {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            session.beginTransaction();
+            session.save(assignment);
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
--- src/main/java/com/submission/mis/filesonlinesubmission/dao/UserDao.java
+++ src/main/java/com/submission/mis/filesonlinesubmission/dao/UserDao.java
@@ -1,4 +1,27 @@
 package com.submission.mis.filesonlinesubmission.dao;
 
+import com.submission.mis.filesonlinesubmission.model.User;
+import org.hibernate.Session;
+import org.hibernate.query.Query;
+
+import org.hibernate.Session;
+import org.hibernate.query.Query;
+
 public class UserDao {
+    public User authenticate(String email, String password) {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            Query<User> query = session.createQuery(
+                    "FROM User WHERE email = :email AND password = :password", User.class
+            );
+            query.setParameter("email", email);
+            query.setParameter("password", password);
+
+            return query.uniqueResult(); // Returns null if no match found
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
 }
+
+
